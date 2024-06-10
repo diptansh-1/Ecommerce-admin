7b0d48bca3bd13affd30b1d4ee21edc3971523af
Diptansh Kumar <139675087+diptansh-1@users.noreply.github.com>
2024-06-10 14:22:26 +0530

    first commit

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useStoreModal } from '@/hooks/use-store-modal';
+import React, { useEffect } from 'react';
+
+
+const SetupPage = () => {
+  const onOpen = useStoreModal((state)=> state.onOpen);
+  const isOpen = useStoreModal((state)=> state.isOpen);
+
+  useEffect(()=>{
+    if (!isOpen){
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+
+
+  return (
+    <div className="p-4">
+      root page
+    </div>
+  );
+}
+
+export default SetupPage;
